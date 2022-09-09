68189573883eb5ac0ec4ae2d085f08c5b91f51e1
Marcola <sbmarcos777@gmail.com>
2022-09-09 01:24:07 -0300

    first commit

---
+++ js/login.js
@@ -0,0 +1,23 @@
+
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+
+// HABILITAR e DESABILITAR BOTÃO "PLAY"
+const validateInput = ({target}) => {
+    if (target.value.length > 3 ){
+        button.removeAttribute('disabled');
+    }else{
+        button.setAttribute('disabled', '')
+    }
+}
+
+// SALVAR NOME DO JOGADOR
+handleSubmit = (event) => {
+    event.preventDefault(); // Remove o "comportamento" padrão do submit
+    localStorage.setItem('player', input.value); // Salva o nome no Local Storage
+    window.location = 'pages/game.html';
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
